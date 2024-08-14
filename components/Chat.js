c8da790862935af9b35ddc8caa57ef7b1b34d051
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-08-14 12:25:08 +0200

    Implement Gifted Chat library and add KeyboardAvoidingView prop for  all devoces

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react'
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react'
+import { View, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
   const { name, backgroundColor } = route.params;
 
   // Set the title and background color for the Chat screen
@@ -18,9 +20,61 @@ const Chat = ({ route, navigation }) => {
     });
   }, []);
 
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "You’ve entered the chat.",
+        createdAt: new Date(),
+        system: true,
+      },
+      {
+        _id: 2,
+        text: 'Hello developer!',
+        createdAt: new Date(),
+        user: {
+          _id: 3,
+          name: 'React Native',
+          avatar: 'https://www.nodemma.com/_next/image?url=%2Fhero-image.png&w=384&q=75'
+        }
+      },
+    ]);
+  }, []);
+
+  // Send a message
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, []);
+
+  // Render the chat bubble setting the background color based on the user
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello Chat!</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      {/*  Keyboard adjustment for Android and iOS*/}
+       { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
     </View>
   )
 }
@@ -29,8 +83,7 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    fontSize: 20,
   }
 });
 
