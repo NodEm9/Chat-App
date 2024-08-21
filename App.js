e70db137ef5095ceaa7ea6b02fa41a0b64f37f0d
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-08-21 15:05:01 +0200

    Implement firebase storage

--- App.js
+++ App.js
@@ -4,64 +4,73 @@ import Start from './components/Start';
 import Chat from './components/Chat';
 import { Alert, LogBox } from 'react-native';
 
-
-// Create the navigator
-const Stack = createNativeStackNavigator();
-
-
 import { initializeApp } from 'firebase/app';
 import { getFirestore, disableNetwork, enableNetwork } from 'firebase/firestore';
-
+import { getStorage } from 'firebase/storage';
 import { useEffect } from 'react';
 import { useNetInfo } from '@react-native-community/netinfo';
 
 
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
 LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
 
 const App = () => {
 
   const connectionStatus = useNetInfo();
 
-  useEffect(() => { 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyALpT8pKtX_a9d_S1LMDeAS2ZLT9BPW_BE",
+    authDomain: "chatapp-90274.firebaseapp.com",
+    projectId: "chatapp-90274",
+    storageBucket: "chatapp-90274.appspot.com",
+    messagingSenderId: "466450113891",
+    appId: "1:466450113891:web:a96d6a96fe8bd360601f56"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Get the Firestore services
+  const db = getFirestore(app);
+
+  // Get the Storage handler service
+  const storage = getStorage(app);
+
+  useEffect(() => {
     if (connectionStatus.isConnected === false) {
       Alert.alert('No Internet Connection');
       disableNetwork(db);
     } else if (connectionStatus.isConnected === true) {
       enableNetwork(db);
     }
-  } , [connectionStatus.isConnected]);
+  }, [connectionStatus.isConnected]);
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyALpT8pKtX_a9d_S1LMDeAS2ZLT9BPW_BE", 
-  authDomain: "chatapp-90274.firebaseapp.com",
-  projectId: "chatapp-90274",
-  storageBucket: "chatapp-90274.appspot.com",
-  messagingSenderId: "466450113891",
-  appId: "1:466450113891:web:a96d6a96fe8bd360601f56"
-};
-
-// Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  
-  const db = getFirestore(app);
 
   return (
     <NavigationContainer>
-    <Stack.Navigator
-      initialRouteName="Start"
-    >
-      <Stack.Screen
-        name="Start"
-        component={Start}
-      />
-      <Stack.Screen
-        name="Chat"
+      <Stack.Navigator
+        initialRouteName="Start"
+      >
+        <Stack.Screen
+          name="Start"
+          component={Start}
+        />
+        <Stack.Screen
+          name="Chat"
         >
-          {props => <Chat isConnected={connectionStatus.isConnected} {...props} db={db} />}
-      </Stack.Screen>
-    </Stack.Navigator>
-  </NavigationContainer>
+          {props => <Chat
+            {...props}
+            isConnected={connectionStatus.isConnected}
+            db={db}
+            storage={storage}
+          />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
